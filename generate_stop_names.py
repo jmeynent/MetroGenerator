bbbae40b797a59c0fb7010c830426d67e64e4c8d
jmeynent <fynchmeynent@gmail.com>
2025-02-13 19:34:04 +0100

    Tokenizing with Mistral

--- generate_stop_names.py
+++ generate_stop_names.py
@@ -1,4 +1,7 @@
 import pandas as pd 
+from huggingface_hub import hf_hub_download
+from transformers import AutoTokenizer,AutoModelForCausalLM
+from huggingface_hub import login
 
 #Import data to have training data
 idf = pd.read_csv('data/idf.csv', sep=";")
@@ -7,4 +10,21 @@ marseille = pd.read_csv('data/marseille.csv', sep=";")
 name_idf = idf['nom_long']
 name_marseille = marseille['Long Name']
 
-names = pd.concat([name_idf, name_marseille])
+names = pd.concat([name_idf, name_marseille], ignore_index=True)
+names = names.to_frame(name="noms")
+
+
+# Load model directly
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-Small-24B-Instruct-2501")
+#model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-Small-24B-Instruct-2501")
+
+if tokenizer.pad_token is None:
+    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    #model.resize_token_embeddings(len(tokenizer))
+    
+batch_size = 1000  # Ajuste selon la RAM dispo
+tokenized_names = []
+
+for i in range(0, len(names), batch_size):
+    batch = names["noms"].iloc[i : i + batch_size].tolist()
+    tokenized_names.extend(tokenizer(batch, padding="max_length", truncation=True)["input_ids"])
